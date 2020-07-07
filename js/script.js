35501ab738d51de682fe878ff766f4b1c11690a5
lindam <linda.minotti2@gmail.com>
2020-07-07 16:31:58 +0200

    Creare nuovi elementi nella lista, attraverso un input.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,36 @@ $(document).ready(function(){
   // Richiama la lista e la stampa a schermo
   getList();
 
+  // Prende il valore dell'input e lo passa all'API
+  $('#submit-item').click(function(){
+    addToList($('#input-item').val());
+  });
+
+  // Funzione che passa all'api il valore dell'input
+  // Accetta: inputValue, stringa
+  // Return: niente, stampa a schermo utilizzando Handlebars
+  function addToList(inputItem){
+    if (inputItem > 0) {
+      $.ajax(
+        {
+          url: 'http://157.230.17.132:3019/todos/',
+          method: 'POST',
+          data: {
+            text: inputItem
+          },
+          success: function(dataResponse){
+            getList();
+          },
+          error: function(){
+            alert('Non è possibile aggiungere cose alla lista!')
+          }
+        }
+      );
+    } else {
+      alert('Devi scrivere qualcosa prima di fare invio!')
+    }
+  };
+
   // Funzione che richiama la lista delle cose da cose da fare
   // e la stampa
   function getList(){
@@ -23,6 +53,8 @@ $(document).ready(function(){
   // Accetta: arrayList, array della lista delle cose da fare
   // Return: niente, stampa a schermo utilizzando Handlebars
   function printList(arrayList){
+    // Pulisca la lista, prima di ristamparla
+    $('.todo-list').text('');
     // Preparo template di Handelbars
     var source = $('#todo-template').html();
     var template = Handlebars.compile(source);
